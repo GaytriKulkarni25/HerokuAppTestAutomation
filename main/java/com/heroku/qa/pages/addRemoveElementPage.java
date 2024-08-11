f7e8604f86c61e89a58b8cd077f09de9d167faf1
Gaytri <gayatrikulkarni_25@hotmail.com>
2024-08-11 15:47:18 +0530

    changes made in addRemoveElementTest.java

--- main/java/com/heroku/qa/pages/addRemoveElementPage.java
+++ main/java/com/heroku/qa/pages/addRemoveElementPage.java
@@ -1,12 +1,15 @@
 package com.heroku.qa.pages;
 
 import com.heroku.qa.base.TestBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class addRemoveElementPage extends TestBase {
 
@@ -30,8 +33,15 @@ public class addRemoveElementPage extends TestBase {
     {
         elementLink.click();
         elementButton.click();
+
+        //Assert.assertEquals(!(deleteButton.isDisplayed()),false);
+         List<WebElement> delButtons=  driver.findElements(By.xpath("//div[@id='elements']/button"));
+         if(delButtons.size()>0)
+             System.out.println("delete button size"+ delButtons.size());
+         else
+             System.out.println("delete button  not found"+ delButtons.size());
         deleteButton.click();
-        Assert.assertEquals(!(deleteButton.isDisplayed()),false);
+       
 
     }
 
